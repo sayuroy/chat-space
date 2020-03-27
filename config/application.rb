d8df624b74854bd02380e25b3ca30fd398c777e7
sayuroy <balotje918@gmail.com>
2020-03-27 17:10:33 +0900

    ajax commit

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'# 日本時間に変更
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
